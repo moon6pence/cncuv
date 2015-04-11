e3ce2327bf170a61b0f373b5e3ac2989e7df6905
khkim2 <moon6pence@gmail.com>
2015-04-12 06:39:35 +0900

    Test libuv work queue

--- test.cpp
+++ test.cpp
@@ -24,6 +24,44 @@ TEST_CASE("Hello libuv", "[libuv]")
     uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 }
 
+long fib_(long t) 
+{
+    if (t == 0 || t == 1)
+        return 1;
+    else
+        return fib_(t - 1) + fib_(t - 2);
+}
+
+void fib(uv_work_t *req)
+{
+    int n = *(int *)req->data;
+    //if (random() % 2)
+    //    sleep(1);
+    //else
+    //    sleep(3);
+
+    long fib = fib_(n);
+    fprintf(stderr, "%dth fibonacci is %lu\n", n, fib);
+}
+
+TEST_CASE("Test work queue", "[libuv]")
+{
+    const int FIB_UNTIL = 42;
+    uv_loop_t *loop = uv_default_loop();
+
+    int data[FIB_UNTIL];
+    uv_work_t req[FIB_UNTIL];
+    int i;
+    for (i = 0; i < FIB_UNTIL; i++) 
+    {
+        data[i] = i;
+        req[i].data = (void *)&data[i];
+        uv_queue_work(loop, &req[i], fib, nullptr);
+    }
+
+    uv_run(loop, UV_RUN_DEFAULT);
+}
+
 #include <cnc/cnc.h>
 
 TEST_CASE("Put and get item", "[ItemCollection]")
